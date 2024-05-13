7ab726b517a06bad2e1515c7bb492f2b64994acb
gitvanya34 <gitvanya34@yandex.ru>
2024-05-13 20:52:28 +0300

    + path

--- SortifyScanAPI/sortifyscan/__init__.py
+++ SortifyScanAPI/sortifyscan/__init__.py
@@ -11,7 +11,7 @@ from sortifyscan.export import ExportMedia
 from sortifyscan.Isortifyscan import ISortifyScan
 
 if __name__ == '__main__':
-    ISortifyScan.sortify_scan("")
+    ISortifyScan.sortify_scan("/content/SortifyScan/ConveyorEmulator/render/0000-0105 - Trim.avi")
     ISortifyScan.sortify_scan_make_video("2024-05-04_23-29-28")
 
 # Длина (0.569м): 0.5537314142752592м; delta 1.5268585724740769см
